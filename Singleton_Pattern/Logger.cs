8a7a9bc12559cbf9560f1e6b426e7213e90ba7bc
mostafa rezaie <mostafa.rezaie@gmail.com>
2024-11-22 02:37:33 +0330

    Add project files.

---
+++ Singleton_Pattern/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    internal class Logger
+    {
+        private static readonly Logger Instance = new Logger();
+        private Logger() { }
+        public static Logger GetInstance()
+        {
+            return Instance;
+        }
+    }
+}
---
+++ Singleton_Pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            Console.WriteLine(logger1.GetHashCode());
+            Console.WriteLine(logger2.GetHashCode());
+            Console.ReadLine();
+
+        }
+    }
+}
